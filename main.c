b6ffa7ddffeeb9be01b6af74ee794577ac1af412
brimohacks <brianmaema28@gmail.com>
2024-01-19 00:10:58 +0300

    Pushed all fixes

--- main.c
+++ main.c
@@ -1,103 +1,87 @@
+#include <stdio.h>
 #include "monty.h"
 
-global_t vglo;
-
+arguments_t *arg = NULL;
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 /**
- * free_vglo - frees the global variables
- *
- * Return: no return
- */
-void free_vglo(void)
-{
-	free_dlistint(vglo.head);
-	free(vglo.buffer);
-	fclose(vglo.fd);
-}
-
-/**
- * start_vglo - initializes the global variables
- *
- * @fd: file descriptor
- * Return: no return
- */
-void start_vglo(FILE *fd)
-{
-	vglo.lifo = 1;
-	vglo.cont = 1;
-	vglo.arg = NULL;
-	vglo.head = NULL;
-	vglo.fd = fd;
-	vglo.buffer = NULL;
-}
-
-/**
- * check_input - checks if the file exists and if the file can
- * be opened
- *
+ * main - program entry
  * @argc: argument count
- * @argv: argument vector
- * Return: file struct
- */
-FILE *check_input(int argc, char *argv[])
-{
-	FILE *fd;
-
-	if (argc == 1 || argc > 2)
-	{
-		dprintf(2, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fd = fopen(argv[1], "r");
-
-	if (fd == NULL)
-	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	return (fd);
-}
-
-/**
- * main - Entry point
+ * @argv: array of arguments availed
  *
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success
+ * Return: EXIT_SUCCESS on success EXIT_FAILURE on fail
  */
 int main(int argc, char *argv[])
 {
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t nlines = 0;
-	char *lines[2] = {NULL, NULL};
+	FILE *mfile;
+	char *line;
+	size_t i = 0;
+	ssize_t x = 1;
+	stack_t *list = NULL;
+	unsigned int y = 0;
 
-	fd = check_input(argc, argv);
-	start_vglo(fd);
-	nlines = getline(&vglo.buffer, &size, fd);
-	while (nlines != -1)
+	if (argc != 2)
 	{
-		lines[0] = _strtoky(vglo.buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
-		{
-			f = get_opcodes(lines[0]);
-			if (!f)
-			{
-				dprintf(2, "L%u: ", vglo.cont);
-				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_vglo();
-				exit(EXIT_FAILURE);
-			}
-			vglo.arg = _strtoky(NULL, " \t\n");
-			f(&vglo.head, vglo.cont);
-		}
-		nlines = getline(&vglo.buffer, &size, fd);
-		vglo.cont++;
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
 
-	free_vglo();
+	arg = malloc(sizeof(arguments_t));
+	if (arg == NULL)
+	{
+		malloc_fail();
+	}
+	arg->l = 0;
+	mfile = fopen(argv[1], "r");
+	arg->montyfile = mfile;
+	if (arg->montyfile == NULL || mfile == NULL)
+	{
+		fileopen(argv[1]);
+	}
 
+	while (x > 0)
+	{
+		line = NULL;
+		x = getline(&line, &i, mfile);
+		arg->line = line;
+		y++;
+		if (x > 0)
+		{
+			getfunc(line, &list, y, mfile);
+		}
+		free(line);
+	}
+	frees(list);
+	fclose(mfile);
+	free(arg);
 	return (0);
+}
+
+/**
+ * frees - frees linked list stack
+ * @h: pointer to head of linked list
+ */
+void frees(stack_t *h)
+{
+	stack_t *a = h;
+
+	if (h == NULL)
+	{
+		return;
+	}
+	while (h != NULL)
+	{
+		a = h;
+		h = h->next;
+		free(a);
+	}
+}
+
+/**
+ * all_free - frees all allocated memory
+ *
+ */
+void all_free(void)
+{
+	fclose(arg->montyfile);
+	free(arg->line);
 }
